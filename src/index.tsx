4afa8447ffa7516d871f5b94de3821f251fc0811
behzad1k <bhzd1k@gmail.com>
2024-05-15 08:07:04 +0330

    sw

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App.tsx';
-
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";``
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Router>
       <App />
     </Router>
 );
+serviceWorkerRegistration.register();
