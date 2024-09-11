114a3c4bdc996231b0e4152a1663146a3a65104d
behzad1k <bhzd1k@gmail.com>
2024-09-11 14:28:08 +0330

    enable tomorrow

--- src/app/website/pages/newOrder/workerStep.tsx
+++ src/app/website/pages/newOrder/workerStep.tsx
@@ -83,7 +83,8 @@ export default function WorkerStep({
     const sections = []
     for (let i = 8; i < 20; i = i + 2) {
       const day = moment().add(calTab, 'day').format('jYYYY/jMM/jDD');
-      const disabled = (schedules && schedules[day] ? schedules[day].includes(i) : false)  || !selected.isUrgent ? (calTab == 0) : false;
+      console.log(schedules && schedules[day] ? schedules[day].includes(i) : false);
+      const disabled = (schedules && schedules[day] ? schedules[day].includes(i) : false)  || (!selected.isUrgent ? (calTab == 0) : false);
       sections.push(<span className={`calSectionsSpan${(selected.time == i && selected.date == day) ? ' selected' : ''} ${disabled ? 'disabled' : ''}`} onClick={() => !disabled && setSelected(prev => ({ ...prev, time: i, date: day }))}>{i} - {i + 2}</span>)
     }
 
