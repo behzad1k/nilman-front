0630b8d2393fb5540a7144cebfae69acc51a1f1e
behzad 1k <bhzd1k@gmail.com>
2025-04-24 12:26:35 +0330

    fixes

--- src/config/app-version.ts
+++ src/config/app-version.ts
@@ -1,3 +1,3 @@
 // This file contains the version information used across the application
-export const APP_VERSION = 'v1.2.3';
+export const APP_VERSION = 'v1.5.0';
 export const CACHE_NAME = `app-cache-${APP_VERSION}`;
