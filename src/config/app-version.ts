108e7e38ae96982d461e8d686db4c50ac6dbcc7a
behzad 1k <bhzd1k@gmail.com>
2025-06-07 20:55:34 +0330

    new icons

--- src/config/app-version.ts
+++ src/config/app-version.ts
@@ -1,3 +1,3 @@
 // This file contains the version information used across the application
-export const APP_VERSION = 'v1.6.44';
+export const APP_VERSION = 'v1.6.45';
 export const CACHE_NAME = `app-cache-${APP_VERSION}`;
